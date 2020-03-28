afa6af61a5a1c92b6b663e7e730293f5a8f55e68
al <apizano@purdue.edu>
2020-03-28 02:31:27 -0500

    beginning hackerrank interface problem

---
+++ Interface/src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    interface AdvancedArithmetic{
+        int divisor_sum(int n);
+    }
+
+//Write your code here
+
+//    class MyCalculator implements AdvancedArithmetic {
+//        int divisor_sum(int n) {
+//
+//        }
+//    }
+    public static void main(String[] args) {
+
+    }
+}
