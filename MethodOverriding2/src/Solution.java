48007dd8ff90727f14780d2b4f44ba544659d4ed
al <apizano@purdue.edu>
2020-03-28 18:21:28 -0500

    completed method overriding 2 on hackerrank

--- MethodOverriding2/src/Solution.java
+++ MethodOverriding2/src/Solution.java
@@ -12,7 +12,7 @@ class MotorCycle extends BiCycle{
     MotorCycle(){
         System.out.println("Hello I am a motorcycle, I am "+ define_me());
 
-        String temp=define_me(); //Fix this line
+        String temp=super.define_me(); //Fix this line
 
         System.out.println("My ancestor is a cycle who is "+ temp );
     }
