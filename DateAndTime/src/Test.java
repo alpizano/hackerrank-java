375e9a6041bc586954a0f00ac55a24fd8484d61a
al <apizano@purdue.edu>
2020-03-27 23:56:03 -0500

    using calendar abstract class

--- DateAndTime/src/Test.java
+++ DateAndTime/src/Test.java
@@ -10,24 +10,27 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result {
-
-    /*
-     * Complete the 'findDay' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER month
-     *  2. INTEGER day
-     *  3. INTEGER year
-     */
+class Test {
 
     public static String findDay(int month, int day, int year) {
         Calendar cal = new Calendar.Builder().setDate(year,month,day).build();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year,month,day);
-        int int_day = calendar.get(Calendar.DAY_OF_WEEK);
+        //System.out.println(cal.get(Calendar.DAY_OF_WEEK_IN_MONTH));
 
+        //System.out.println(cal);
+
+       Calendar calendar = Calendar.getInstance();
+
+        //calendar.set(year,month,day);
+        calendar.set(2015,8,5);
+
+//        calendar.set(Calendar.MONTH, month);
+//        calendar.set(Calendar.DATE, day);
+//        calendar.set(Calendar.YEAR, year);
+
+        System.out.println(calendar.toString());
+        int int_day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        System.out.println("the day returned is" + int_day);
         String str_day = "";
         if(int_day == 1) {
             str_day = "Sunday";
@@ -51,30 +54,11 @@ class Result {
             str_day = "Saturday";
         }
         return str_day;
+        }
 
+        public static void main(String[] args) {
+            //Test.findDay(8,5,2015);
+            System.out.println(Test.findDay(8,5,2015));
 
         }
-}
-
-public class Test {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int month = Integer.parseInt(firstMultipleInput[0]);
-
-        int day = Integer.parseInt(firstMultipleInput[1]);
-
-        int year = Integer.parseInt(firstMultipleInput[2]);
-
-        String res = Result.findDay(month, day, year);
-
-        bufferedWriter.write(res);
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
 }
