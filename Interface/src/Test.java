8bd43b7093634b8fb82139b13901359daba4cc52
al <apizano@purdue.edu>
2020-03-28 02:55:53 -0500

    completed Interface question on Hackerrank

--- Interface/src/Test.java
+++ Interface/src/Test.java
@@ -1,16 +1,30 @@
-public class Test {
-    interface AdvancedArithmetic{
+interface AdvancedArithmetic {
         int divisor_sum(int n);
     }
 
 //Write your code here
+class MyCalculator implements AdvancedArithmetic {
+       public int divisor_sum(int n) {
+            int divisor = 1;
+            int sum = 0;
 
-//    class MyCalculator implements AdvancedArithmetic {
-//        int divisor_sum(int n) {
-//
-//        }
-//    }
-    public static void main(String[] args) {
+            while(divisor <= n) {
+                if (n % divisor == 0) {
+                    sum = sum + divisor;
+                }
+                divisor++;
+            }
 
+            return sum;
+        }
     }
-}
+
+    class Test {
+    public static void main(String[] args) {
+        MyCalculator calc = new MyCalculator();
+        System.out.println(calc.divisor_sum(6));
+    }
+    }
+
+
+
