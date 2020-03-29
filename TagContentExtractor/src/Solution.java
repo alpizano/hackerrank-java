4ef1fc9efebf547ca53c8fa2339d6a3d6d112f43
al <apizano@purdue.edu>
2020-03-28 21:48:01 -0500

    completed tag content extractor hackerrank problem

--- TagContentExtractor/src/Solution.java
+++ TagContentExtractor/src/Solution.java
@@ -7,14 +7,35 @@ import java.util.regex.*;
 public class Solution{
     public static void main(String[] args){
 
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        while(testCases>0){
-            String line = in.nextLine();
+//        < h1 > Nayeem loves counseling</h1 >
+//        <h1 ><h1 > Sanjay has no watch</h1 ></h1 ><par > So wait for a while</par >
+//        <Amee > safat codes like a ninja</amee >
+//        <SA premium>Imtiaz has a secret crush</SA premium>
+
+       // Scanner in = new Scanner(System.in);
+        //int testCases = Integer.parseInt(in.nextLine());
+       // while(testCases>0){
+            //String line = in.nextLine();
 
             //Write your code here
+           // line.matches("^>(A-Za-z_0-9)<");
 
-            testCases--;
-        }
+        String s1 = "<h1>Nayeem loves counseling</h1>";
+        String s2 = "<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>";
+        String s3 = "<Amee>safat codes like a ninja</amee>";
+        String s4 = "<SA premium>Imtiaz has a secret crush</SA premium>";
+
+            Pattern p = Pattern.compile("<(.+)>([^<]+)</\\1>");
+           Matcher m = p.matcher(s1);
+
+            if(m.find()) {
+                System.out.println("group(0): " + m.group(0));
+                System.out.println("group(1): " + m.group(1));
+                System.out.println("group(2): " + m.group(2));
+
+            }
+
+           // testCases--;
+        //}
     }
 }
